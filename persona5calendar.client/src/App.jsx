6e449023df8f23110879ecda8475126372233c42
jonesnil <54556584+jonesnil@users.noreply.github.com>
2026-05-29 22:11:52 -0400

    Add json calendar data and browsing
    This commit adds a json file with a basic month and a single event to display in it. Trying to work out the format I want to store and retrieve the data.
    
    It also adds the functionality to display the data on the website, and a tiny bit of formatting to make it look okay though that is not my focus yet.
    
    I'm trying to make it versatile for features I have planned in the future, and also keep as little state as possible and mostly just read the json file, so everything is stored by react as indices in the file.
    
    Normally this stuff would be in a database and not a json file obviously, but I want to eventually host this as a static website (because they're way cheaper and easier to host for a hobbyist) and ultimately the data is all static anyway, so json is fine.

--- persona5calendar.client/src/App.jsx
+++ persona5calendar.client/src/App.jsx
@@ -2,16 +2,35 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import '../Content/bootstrap.css'
 import '../Scripts/bootstrap.min.js'
+import calendarData from './Persona5RoyalCalendarInfo.json'
+function CalendarEvent({ event }) {
+    return <li className="list-group-item">{event["Title"]}</li>
+}
 
-function CalendarDay() {
-    return <td>test</td>
+function CalendarDay({ selectedMonthIndex, weekIndex, dayIndex, onCalendarDayClick }) {
+    var day = calendarData["Months"][selectedMonthIndex]["Weeks"][weekIndex][dayIndex];
+
+    return (
+        <td className={day["OutOfMonth"] ? "outOfMonth" : ""} onClick={onCalendarDayClick}>
+            <span style={{ float: "right", display: "block" }} >{day["Date"]}</span>
+            {("Events" in day && day["Events"].length > 0) ?
+                <ul className="list-group fs-6" style={{ clear: "both", display: "block" }}>
+                    {day["Events"].map((event) =>
+                        <CalendarEvent key={event["Title"]} event={event}></CalendarEvent>)}
+                </ul> : ""
+            }
+        </td>)
 }
 
 function NavBar() {
     return <span className="navBar">~ March 2016 ~</span>
 }
 
-function Calendar() {
+function Calendar({ onDetailsClick, selectedMonthIndex }) {
+    function handleClick(weekIndex, dayIndex) {
+        onDetailsClick(weekIndex, dayIndex);
+    }
+
     return <table className="table calendar">
             <thead>
                 <tr>
@@ -24,59 +43,74 @@ function Calendar() {
                     <th>SAT</th>
                 </tr>
             </thead>
-            <tbody>
-            <tr>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-        </tr>
-        <tr>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-        </tr>
-        <tr>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-        </tr>
-        <tr>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            <CalendarDay></CalendarDay>
-            </tr>
+        <tbody>
+            {calendarData["Months"][selectedMonthIndex]["Weeks"].map((week, weekIndex) =>
+                <tr key={weekIndex}>
+                    {week.map((day, dayIndex) => <CalendarDay key={dayIndex} selectedMonthIndex={selectedMonthIndex} weekIndex={weekIndex} dayIndex={dayIndex} onCalendarDayClick={() => handleClick(weekIndex, dayIndex)}></CalendarDay>)}
+                </tr>
+                )}
         </tbody>
     </table>
 }
 
-function Details() {
+function Details({ onClickPreviousDay, onClickNextDay, selectedMonthIndex, selectedWeekIndex, selectedDayIndex }) {
+    var selectedDay = calendarData["Months"][selectedMonthIndex]["Weeks"][selectedWeekIndex][selectedDayIndex];
+
     return <div className="card details">
-            <div className="card-body">
-                This is some text within a card body.
+        <div className="card-body">
+            <h5 className="card-title text-center">
+                <button className="btn btn-secondary me-2 float-start" onClick={onClickPreviousDay}>{"<"}</button>
+                {selectedDay["WeekDay"]}&nbsp;
+                {calendarData["Months"][selectedMonthIndex]["Month"]}&nbsp;
+                {selectedDay["Date"]}
+                <button className="btn btn-secondary ms-2 float-end" onClick={onClickNextDay}>{">"}</button>
+            </h5>
+            {("Events" in selectedDay && selectedDay["Events"].length > 0) ?
+                <ul className="list-group fs-6" style={{ clear: "both", display: "block" }}>
+                    {selectedDay["Events"].map((event) =>
+                        <CalendarEvent key={event["Title"]} event={event}></CalendarEvent>)}
+                </ul> : ""
+            }
             </div>
            </div>
 }
 
 export default function App() {
-    return <div className="calendarLayout">
-            <NavBar></NavBar>
-            <Calendar></Calendar>
-            <Details></Details>
-           </div>
+
+    const [selectedMonthIndex, setSelectedMonthIndex] = useState(0);
+    const [selectedWeekIndex, setSelectedWeekIndex] = useState(0);
+    const [selectedDayIndex, setSelectedDayIndex] = useState(0);
+    function displayDetails(weekIndex, dayIndex) {
+        setSelectedWeekIndex(weekIndex);
+        setSelectedDayIndex(dayIndex);
+    }
+
+    function iterateSelectedDay(next) {
+        var weeksInMonth = calendarData["Months"][selectedMonthIndex]["Weeks"];
+        var selectedWeek = weeksInMonth[selectedWeekIndex];
+        if (next) {
+            if (selectedWeek.length - 1 > selectedDayIndex) {
+                setSelectedDayIndex(selectedDayIndex + 1);
+            }
+            else if (weeksInMonth.length - 1 > selectedWeekIndex) {
+                setSelectedWeekIndex(selectedWeekIndex + 1);
+                setSelectedDayIndex(0);
+            }
+        }
+        else {
+            if (selectedDayIndex > 0) {
+                setSelectedDayIndex(selectedDayIndex - 1);
+            }
+            else if (selectedWeekIndex > 0) {
+                setSelectedWeekIndex(selectedWeekIndex - 1);
+                setSelectedDayIndex(weeksInMonth[selectedWeekIndex].length - 1);
+            }
+        }
+    }
+
+    return (<div className="calendarLayout">
+        <NavBar></NavBar>
+        <Calendar onDetailsClick={displayDetails} selectedMonthIndex={selectedMonthIndex}></Calendar>
+        <Details onClickPreviousDay={() => iterateSelectedDay(false)} onClickNextDay={() => iterateSelectedDay(true)} selectedMonthIndex={selectedMonthIndex} selectedWeekIndex={selectedWeekIndex} selectedDayIndex={selectedDayIndex}></Details>
+            </div>)
 }
