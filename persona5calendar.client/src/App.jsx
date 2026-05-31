2d3612a05eab6112112eefc7843a1fa7acca915f
jonesnil <54556584+jonesnil@users.noreply.github.com>
2026-05-31 18:04:39 -0400

    Details display additions + month/day iteration stuff
    This update adds:
    -weekly events
    --for stuff that's available when there is freetime on a certain weekday
    -weather event details
    --now the selected day will have a description of what's special about any given weather status on it
    -you can use arrow keys/wasd to iterate over selected days
    -new month of november added
    -you can iterate over months now, with both day buttons/keys and also a new set of month iterator buttons on top of the screen
    
    Update also changes:
    -the css for the restricted/blocked/free time slots has been changed to make it a bit easier to understand. now the gradient bar maps directly to the names of the dayslot statuses
    -added a color for restricted time (though status is not fully implemented for all class combinations)
    -when you select a day that's out of the month you're looking at, it will now  check to see if that day exists in the previous month, and if so it will display those details

--- persona5calendar.client/src/App.jsx
+++ persona5calendar.client/src/App.jsx
@@ -75,6 +75,138 @@ function CalendarEventDetails({ event }) {
             </li>)
 }
 
+function WeeklyEventDetails({ day }) {
+    var headerText;
+    var detailedText;
+
+    if (day["DaySlot"] === "Free" || day["NightSlot"] === "Free") {
+        switch (day["WeekDay"]) {
+            case 'Sunday':
+                headerText = "Aojiru";
+                detailedText = "Visit the drink stand in the Shibuya Underground Walkway to buy a Social Stat raising drink (does not spend free time!)";
+                break;
+        }
+    }
+
+    if (headerText) {
+        return <div className="mb-3">
+            <h4>{headerText}</h4>
+            <span>{detailedText}</span>
+        </div>
+    }
+    return (null)
+}
+
+function WeatherDetails({ day, dayTime }) {
+    var headerText;
+    var statusEffects = [];
+    var weatherSlot = day["WeatherDay"];
+    var timeSlot = day["DaySlot"];
+
+    if (!dayTime) {
+        weatherSlot = day["WeatherNight"];
+        timeSlot = day["NightSlot"];
+    }
+
+                    statusEffects.push("Mementos Effect: Higher chance of rare/treasure shadows");
+    if (timeSlot === "Free") {
+        switch (weatherSlot) {
+            case 'Rain':
+                if (dayTime) {
+                    headerText = "Rainy Day";
+                    statusEffects.push("Studying at Diner gives +3 Knowledge");
+                }
+                else {
+                    headerText = "Rainy Night";
+                    statusEffects.push("Bathhouse gives +3 Charm and +1 Guts");
+                    statusEffects.push("Studying at Leblanc gives +3 Knowledge");
+                }
+                break;
+            case 'Torrential Rain':
+                if (dayTime) {
+                    headerText = "Torrential Rain";
+                    statusEffects.push("Studying at Diner gives +3 Knowledge");
+                    statusEffects.push("Mementos Effect: More treasure chests");
+                }
+                else {
+                    headerText = "Rainy Night";
+                    statusEffects.push("Bathhouse gives +3 Charm and +1 Guts");
+                    statusEffects.push("Studying at Leblanc gives +3 Knowledge");
+                }
+                break;
+            case 'Snow':
+                if (dayTime) {
+                    headerText = "Snowy Day";
+                    statusEffects.push("Studying at Diner gives +3 Knowledge");
+                }
+                else {
+                    headerText = "Snowy Night";
+                    statusEffects.push("Bathhouse gives +3 Charm and +1 Guts");
+                    statusEffects.push("Studying at Leblanc gives +3 Knowledge");
+                }
+                break;
+            case 'Flu Season':
+                if (dayTime) {
+                    headerText = "Flu Season";
+                    statusEffects.push("Mementos Effect: Shadows may start combat with Despair");
+                }
+                break;
+            case 'Pollen':
+                if (dayTime) {
+                    headerText = "Pollen Warning";
+                    statusEffects.push("Mementos Effect: Shadows may start combat Asleep");
+                }
+                break;
+            case 'Heat Wave':
+                if (dayTime) {
+                    headerText = "Heat Wave";
+                    statusEffects.push("Studying at Diner gives +3 Knowledge");
+                    statusEffects.push("Mementos Effect: Shadows may start combat with Burn");
+                }
+                else {
+                    headerText = "Tropical Night";
+                    statusEffects.push("Bathhouse gives +3 Charm and +1 Guts");
+                    statusEffects.push("Studying at Leblanc gives +3 Knowledge");
+                }
+                break;
+            case 'Tropical Night':
+                if (dayTime) {
+                    headerText = "Heat Wave";
+                    statusEffects.push("Studying at Diner gives +3 Knowledge");
+                    statusEffects.push("Mementos Effect: Shadows may start combat with Burn");
+                }
+                else {
+                    headerText = "Tropical Night";
+                    statusEffects.push("Bathhouse gives +3 Charm and +1 Guts");
+                    statusEffects.push("Studying at Leblanc gives +3 Knowledge");
+                }
+                break;
+            case 'Cold Wave':
+                if (dayTime) {
+                    headerText = "Cold Day";
+                    statusEffects.push("Studying at Diner gives +3 Knowledge");
+                    statusEffects.push("Mementos Effect: Shadows may start combat with Freeze");
+                }
+                else {
+                    headerText = "Cold Night";
+                    statusEffects.push("Bathhouse gives +3 Charm and +1 Guts");
+                    statusEffects.push("Studying at Leblanc gives +3 Knowledge");
+                }
+                break;
+        }
+    }
+
+    if (headerText) {
+        return <div className="mb-3">
+            <h4>{headerText}</h4>
+            <ul>
+                {statusEffects.map((status, index) => <li key={index}>{status}</li>)}
+            </ul>
+        </div>
+    }
+    return (null)
+}
+
 function FreeTimeDetails({ day }) {
     var headerText;
     var detailedText;
@@ -82,6 +214,10 @@ function FreeTimeDetails({ day }) {
         day["NightSlot"] === "Blocked") {
         headerText = "Busy Day!";
         detailedText = "Joker will have no free time.";
+    }
+    else if (day["DaySlot"] === "Blocked" && day["NightSlot"] === "Restricted") {
+        headerText = "Night in...";
+        detailedText = "Joker will only have free time at night, and he must stay in Leblanc.";
     }
     else if (day["DaySlot"] === "Blocked") {
         headerText = "Busy Afternoon!";
@@ -119,26 +255,7 @@ function CalendarDay({ windowDimensions, selectedMonthIndex, selectedWeekIndex, 
         dateNumberClasses += "calendarDateNumber arsenal-bold ";
     }
 
-    var dayOverviewClasses = "dayOverviewDiv mb-2 ";
-    if (day["DaySlot"] === "Free" &&
-        day["NightSlot"] === "Free") {
-        dayOverviewClasses += " freeTime ";
-    }
-    else if (day["DaySlot"] === "Free" &&
-        day["NightSlot"] === "Blocked") {
-        dayOverviewClasses += " freeTimeNightOnly ";
-
-    }
-    else if (day["DaySlot"] === "Blocked" &&
-        day["NightSlot"] === "Free") {
-        dayOverviewClasses += " freeTimeDayOnly ";
-
-    }
-    else if (day["DaySlot"] === "Blocked" &&
-        day["NightSlot"] === "Blocked") {
-        dayOverviewClasses += " blockedTime ";
-
-    }
+    var dayOverviewClasses = `dayOverviewDiv mb-2 ${day["DaySlot"]}${day["NightSlot"]}`;
 
     return (
         <td className={cellClasses} onClick={onCalendarDayClick}>
@@ -160,12 +277,19 @@ function CalendarDay({ windowDimensions, selectedMonthIndex, selectedWeekIndex, 
         </td>)
 }
 
-function NavBar({ selectedMonthIndex }) {
+function NavBar({ onClickPreviousMonth, onClickNextMonth, selectedMonthIndex }) {
     var month = calendarData["Months"][selectedMonthIndex];
 
     return <h1 className="navBar text-center libre-franklin-bold">
-        ~<span className="boxedText" style={{ transform: "rotate(-2deg)" }}>{month["Month"]}</span>&nbsp;
-         <span className="boxedText" style={{ transform: "rotate(1deg)" }}>{month["Year"]}</span>~</h1>
+        <button className="btn btn-dark me-2" onClick={onClickPreviousMonth}>
+            <span className="material-symbols-outlined">line_start_arrow_notch</span>
+        </button>
+         <span className="boxedText" style={{ transform: "rotate(-2deg)" }}>{month["Month"]}</span>&nbsp;
+        <span className="boxedText" style={{ transform: "rotate(1deg)" }}>{month["Year"]}</span>
+        <button className="btn btn-dark ms-2" onClick={onClickNextMonth}>
+         <span className="material-symbols-outlined">line_end_arrow_notch</span>
+        </button>
+        </h1>
 }
 
 function Calendar({ onDetailsClick, windowDimensions, selectedMonthIndex, selectedWeekIndex, selectedDayIndex }) {
@@ -204,56 +328,118 @@ function Calendar({ onDetailsClick, windowDimensions, selectedMonthIndex, select
     </table>
 }
 
-function DetailsNavbar({ selectedDay, selectedMonthIndex, onClickNextDay, onClickPreviousDay, windowDimensions }) {
+function DetailsNavbar({ day, month, onClickNextDay, onClickPreviousDay, windowDimensions }) {
     var dayText;
     var monthText;
-    var month = calendarData["Months"][selectedMonthIndex];
     if (windowDimensions.width > 768 && windowDimensions.width <= 1024) {
-        monthText = selectedDay["OutOfMonth"] ? selectedDay["OutOfMonthNum"] : month["Number"];
-        dayText = `${monthText}/${selectedDay["Date"]}`;
+        monthText = day["OutOfMonth"] ? day["OutOfMonthNum"] : month["Number"];
+        dayText = `${monthText}/${day["Date"]}`;
     }
     else {
-        monthText = selectedDay["OutOfMonth"] ? selectedDay["OutOfMonth"] : month["Month"];
-        dayText = `${selectedDay["WeekDay"]} ${monthText} ${selectedDay["Date"]}`;
+        monthText = day["OutOfMonth"] ? day["OutOfMonth"] : month["Month"];
+        dayText = `${day["WeekDay"]} ${monthText} ${day["Date"]}`;
     }
     return <h5 className="card-title text-center mb-4 libre-franklin-regular flexSpaceBetween">
-                <button className="btn btn-danger" onClick={onClickPreviousDay}>{"<"}</button>
+                <button className="btn btn-danger" onClick={onClickPreviousDay}>
+                    <span className="material-symbols-outlined">line_start_arrow_notch</span>
+                </button>
                 <span>{dayText}</span>
-                <button className="btn btn-danger" onClick={onClickNextDay}>{">"}</button>
+                <button className="btn btn-danger" onClick={onClickNextDay}>
+                    <span className="material-symbols-outlined">line_end_arrow_notch</span>
+                </button>
             </h5>
 }
 
 function Details({ onClickPreviousDay, onClickNextDay, windowDimensions, selectedMonthIndex, selectedWeekIndex, selectedDayIndex }) {
+    var selectedMonth = calendarData["Months"][selectedMonthIndex];
     var selectedDay = calendarData["Months"][selectedMonthIndex]["Weeks"][selectedWeekIndex][selectedDayIndex];
+
+    if (selectedDay["OutOfMonth"]) {
+        if (selectedDay["OutOfMonthNum"] < selectedMonth["Number"]
+            && selectedMonthIndex > 0) {
+            var previousMonth = calendarData["Months"][selectedMonthIndex - 1];
+            var numberOfWeeksInPreviousMonth = previousMonth["Weeks"].length;
+            selectedDay = previousMonth["Weeks"][numberOfWeeksInPreviousMonth - 1][selectedDayIndex];
+            selectedMonth = previousMonth;
+        }
+        if (selectedDay["OutOfMonthNum"] > selectedMonth["Number"]
+            && selectedMonthIndex < calendarData["Months"].length - 1) {
+            var nextMonth = calendarData["Months"][selectedMonthIndex + 1];
+            selectedDay = nextMonth["Weeks"][0][selectedDayIndex];
+            selectedMonth = nextMonth;
+        }
+    }
 
     return <div className="card details">
         <div className="card-body">
-            <DetailsNavbar selectedDay={selectedDay} selectedMonthIndex={selectedMonthIndex} onClickNextDay={onClickNextDay} onClickPreviousDay={onClickPreviousDay} windowDimensions={windowDimensions}></DetailsNavbar>
+            <DetailsNavbar day={selectedDay} month={selectedMonth} onClickNextDay={onClickNextDay} onClickPreviousDay={onClickPreviousDay} windowDimensions={windowDimensions}></DetailsNavbar>
+            <WeatherDetails day={selectedDay} dayTime={true}></WeatherDetails>
+            <WeatherDetails day={selectedDay} dayTime={false}></WeatherDetails>
             <FreeTimeDetails day={selectedDay}></FreeTimeDetails>
             {("Events" in selectedDay && selectedDay["Events"].length > 0) ?
-                <ul className="list-group fs-6 arsenal-regular" style={{ clear: "both", display: "block" }}>
+                <ul className="list-group fs-6 arsenal-regular mb-2" style={{ clear: "both", display: "block" }}>
                     {selectedDay["Events"].map((event) =>
                         <CalendarEventDetails key={event["Title"]} event={event}></CalendarEventDetails>)}
                 </ul> : ""
             }
+            <WeeklyEventDetails day={selectedDay}></WeeklyEventDetails>
             </div>
            </div>
 }
 
+var backPressedDownLast = 0;
+var forwardPressedDownLast = 0;
 export default function App() {
 
     const [selectedMonthIndex, setSelectedMonthIndex] = useState(0);
     const [selectedWeekIndex, setSelectedWeekIndex] = useState(0);
     const [selectedDayIndex, setSelectedDayIndex] = useState(0);
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    const [backPressed, setBackPressed] = useState(false);
+    const [forwardPressed, setForwardPressed] = useState(false);
 
     useEffect(() => {
+        const handleKeyDown = (event) => {
+            // Check if the pressed key is 'left'
+            if (event.keyCode === 37 || event.keyCode === 65) {
+                if (Date.now()  - backPressedDownLast > 200) {
+                    setBackPressed(true);
+                    backPressedDownLast = Date.now();
+                }
+            }
+            // Check if the pressed key is 'right'
+            if (event.keyCode === 39 || event.keyCode === 68) {
+                if (Date.now() - forwardPressedDownLast > 200) {
+                    setForwardPressed(true);
+                    forwardPressedDownLast = Date.now();
+                }
+            }
+        };
+
+        const handleKeyUp = (event) => {
+            // Check if the pressed key is 'left'
+            if (event.keyCode === 37 || event.keyCode === 65) {
+                backPressedDownLast = 0;
+            }
+            // Check if the pressed key is 'right'
+            if (event.keyCode === 39 || event.keyCode === 68) {
+                forwardPressedDownLast = 0;
+            }
+        };
+
+
         function handleResize() {
             setWindowDimensions(getWindowDimensions());
         }
 
         window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
+        window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
+        }
     }, []);
 
     function displayDetails(weekIndex, dayIndex) {
@@ -261,15 +447,46 @@ export default function App() {
         setSelectedDayIndex(dayIndex);
     }
 
+    function iterateSelectedMonth(next) {
+        var months = calendarData["Months"];
+        if (next) {
+            if (months.length - 1 > selectedMonthIndex) {
+                setSelectedMonthIndex(selectedMonthIndex + 1);
+            }
+        }
+        else {
+            if (selectedMonthIndex > 0) {
+                setSelectedMonthIndex(selectedMonthIndex - 1);
+            }
+        }
+    }
+
+    function iterateSelectedMonthButtonClick(next) {
+        iterateSelectedMonth(next);
+        setSelectedWeekIndex(0);
+        setSelectedDayIndex(0);
+    }
     function iterateSelectedDay(next) {
         var weeksInMonth = calendarData["Months"][selectedMonthIndex]["Weeks"];
         var selectedWeek = weeksInMonth[selectedWeekIndex];
+        var selectedDay = selectedWeek[selectedDayIndex];
+
         if (next) {
             if (selectedWeek.length - 1 > selectedDayIndex) {
                 setSelectedDayIndex(selectedDayIndex + 1);
             }
             else if (weeksInMonth.length - 1 > selectedWeekIndex) {
                 setSelectedWeekIndex(selectedWeekIndex + 1);
+                setSelectedDayIndex(0);
+            }
+            else if (calendarData["Months"].length - 1 > selectedMonthIndex) {
+                iterateSelectedMonth(true);
+                if (selectedDay["OutOfMonth"]) {
+                    setSelectedWeekIndex(1);
+                }
+                else { 
+                    setSelectedWeekIndex(0);
+                }
                 setSelectedDayIndex(0);
             }
         }
@@ -281,11 +498,32 @@ export default function App() {
                 setSelectedWeekIndex(selectedWeekIndex - 1);
                 setSelectedDayIndex(weeksInMonth[selectedWeekIndex].length - 1);
             }
+            else if (selectedMonthIndex > 0) {
+                iterateSelectedMonth(false);
+                var newWeeksInMonth = calendarData["Months"][selectedMonthIndex]["Weeks"];
+                if (selectedDay["OutOfMonth"]) {
+                    setSelectedWeekIndex(newWeeksInMonth.length - 2);
+                }
+                else {
+                    setSelectedWeekIndex(newWeeksInMonth.length - 1);
+                }
+                setSelectedDayIndex(6);
+            }
         }
     }
 
+    if (backPressed) {
+        iterateSelectedDay(false);
+        setBackPressed(false);
+    }
+
+    if (forwardPressed) {
+        iterateSelectedDay(true);
+        setForwardPressed(false);
+    }
+
     return (<div className="calendarLayout" data-bs-theme="dark">
-        <NavBar selectedMonthIndex={selectedMonthIndex}></NavBar>
+        <NavBar onClickPreviousMonth={() => iterateSelectedMonthButtonClick(false)} onClickNextMonth={() => iterateSelectedMonthButtonClick(true)} selectedMonthIndex={selectedMonthIndex}></NavBar>
         <Calendar onDetailsClick={displayDetails} windowDimensions={windowDimensions} selectedMonthIndex={selectedMonthIndex} selectedWeekIndex={selectedWeekIndex} selectedDayIndex={selectedDayIndex}></Calendar>
         <Details onClickPreviousDay={() => iterateSelectedDay(false)} windowDimensions={windowDimensions} onClickNextDay={() => iterateSelectedDay(true)} selectedMonthIndex={selectedMonthIndex} selectedWeekIndex={selectedWeekIndex} selectedDayIndex={selectedDayIndex}></Details>
             </div>)
