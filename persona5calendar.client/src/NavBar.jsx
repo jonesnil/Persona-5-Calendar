d3021243119b7353cb237ba619c6213369706c00
jonesnil <54556584+jonesnil@users.noreply.github.com>
2026-06-14 21:20:18 -0400

    Added info modal

--- persona5calendar.client/src/NavBar.jsx
+++ persona5calendar.client/src/NavBar.jsx
@@ -5,10 +5,14 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Modal from 'react-bootstrap/Modal';
 import calendarData from './Persona5RoyalCalendarInfo.json'
 function NavBar({ onMonthSelect, selectedMonthIndex, windowDimensions, displayExamEvents, displayJazzClubEvents, displayPuzzleEvents, displayStoryEvents, displayClassEvents, displayWeatherDetails, displayFreeTimeDetails, setDisplayExamEvents, setDisplayJazzClubEvents, setDisplayPuzzleEvents, setDisplayStoryEvents, setDisplayClassEvents, setDisplayWeatherDetails, setDisplayFreeTimeDetails }) {
-    const [show, setShow] = useState(false);
+    const [showSettings, setShowSettings] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleCloseSettings = () => setShowSettings(false);
+    const handleShowSettings = () => setShowSettings(true);
+
+    const handleCloseInfo = () => setShowInfo(false);
+    const handleShowInfo = () => setShowInfo(true);
 
     return <h1 className="navBar text-center libre-franklin-bold" style={{ display: "flex", justifyContent: "space-between" }}>
         <span className="boxedText ms-2" style={{ transform: "rotate(1deg)" }}>{windowDimensions.width <= 768 ? "" : "P5R Planner"}</span>
@@ -18,10 +22,15 @@ function NavBar({ onMonthSelect, selectedMonthIndex, windowDimensions, displayEx
             )}
         </Form.Select>
         <div></div>
-        <Button variant="dark" className="me-2 mt-1" onClick={handleShow}>
-            <span className="material-symbols-outlined">settings</span>
-        </Button>
-        <EventOptionsModal show={show} handleClose={handleClose}
+        <span>
+            <Button variant="dark" className="me-1 mt-1" onClick={handleShowInfo}>
+                <span className="material-symbols-outlined">info</span>
+            </Button>
+            <Button variant="dark" className="me-2 mt-1" onClick={handleShowSettings}>
+                <span className="material-symbols-outlined">settings</span>
+            </Button>
+        </span>
+        <EventOptionsModal showSettings={showSettings} handleCloseSettings={handleCloseSettings}
             displayExamEvents={displayExamEvents} setDisplayExamEvents={setDisplayExamEvents}
             displayStoryEvents={displayStoryEvents} setDisplayStoryEvents={setDisplayStoryEvents}
             displayJazzClubEvents={displayJazzClubEvents} setDisplayJazzClubEvents={setDisplayJazzClubEvents}
@@ -29,13 +38,14 @@ function NavBar({ onMonthSelect, selectedMonthIndex, windowDimensions, displayEx
             displayClassEvents={displayClassEvents} setDisplayClassEvents={setDisplayClassEvents}
             displayWeatherDetails={displayWeatherDetails} setDisplayWeatherDetails={setDisplayWeatherDetails}
             displayFreeTimeDetails={displayFreeTimeDetails} setDisplayFreeTimeDetails={setDisplayFreeTimeDetails}></EventOptionsModal>
+        <InfoModal showInfo={showInfo} handleCloseInfo={handleCloseInfo}></InfoModal>
     </h1>
 }
 
-function EventOptionsModal({ show, handleClose, displayExamEvents, displayJazzClubEvents, displayPuzzleEvents, displayStoryEvents, displayClassEvents, displayWeatherDetails, displayFreeTimeDetails, setDisplayExamEvents, setDisplayJazzClubEvents, setDisplayPuzzleEvents, setDisplayStoryEvents, setDisplayClassEvents, setDisplayWeatherDetails, setDisplayFreeTimeDetails }) {
+function EventOptionsModal({ showSettings, handleCloseSettings, displayExamEvents, displayJazzClubEvents, displayPuzzleEvents, displayStoryEvents, displayClassEvents, displayWeatherDetails, displayFreeTimeDetails, setDisplayExamEvents, setDisplayJazzClubEvents, setDisplayPuzzleEvents, setDisplayStoryEvents, setDisplayClassEvents, setDisplayWeatherDetails, setDisplayFreeTimeDetails }) {
     return (
         <>
-            <Modal show={show} onHide={handleClose} data-bs-theme="dark">
+            <Modal show={showSettings} onHide={handleCloseSettings} data-bs-theme="dark">
                 <Modal.Header closeButton>
                 </Modal.Header>
                 <Modal.Body>
@@ -101,6 +111,30 @@ function EventOptionsModal({ show, handleClose, displayExamEvents, displayJazzCl
                                 }} />
                         </ListGroup.Item>
                     </ListGroup>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
+
+function InfoModal({ showInfo, handleCloseInfo }) {
+    return (
+        <>
+            <Modal show={showInfo} onHide={handleCloseInfo} data-bs-theme="dark">
+                <Modal.Header closeButton>
+                    <span className="fs-4 boxedText rotatedTextAlt">LET US START THE GAME.</span>
+                </Modal.Header>
+                <Modal.Body>
+                    This website is a tool to help tricksters on their playthrough of Persona 5 Royal. Unlike a full guide, this site won't tell you what to do on any given day, but instead it lists many things you may want to keep in mind so you can plan your own playthrough.
+                    <h5 className="mt-3 mb-1">How to read a day:</h5>
+                    The colors underneath the dates in the calendar view are meaningful! They tell you how much free time the player will have on a given day. The left side of the bar represents daytime, and the right side of the bar represents night time. Let's look at the below example:
+                    <div className="dayOverviewDiv dayOverviewDemo FreeBlocked mb-2">
+                        <div></div>
+                        <span className="pe-2 calendarDateNumber arsenal-bold" >14</span>
+                    </div>
+                    It looks like the 14th here is gray on the left and red on the right. That means the player will have free time during the day (gray) but none at night (red.) If you see gold under the date, that means that time slot will be free but can only be spent in Leblanc.
+                    <h5 className="mt-3 mb-1">About the site:</h5>
+                    I made this tool as an excuse to learn <a href="https://react.dev/">React</a>. The code is available on <a href="https://github.com/jonesnil/Persona-5-Calendar">GitHub</a>. I hope you find the tool useful! If you have any issues or would like to give feedback, you can email me at <a href="mailto:P5RPlanner@jonesnil.com">P5RPlanner@jonesnil.com</a> 
                 </Modal.Body>
             </Modal>
         </>
